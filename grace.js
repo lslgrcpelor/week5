c6e6cf6d20f997c0be515cd88638ea6e023b6b7c
Leslie Grace Pelor <lppelor@student.apc.edu.ph>
2023-01-23 22:36:13 +0800

    Bom Window

--- grace.js
+++ grace.js
@@ -1,5 +1,10 @@
-//Bom Window [1]
+
+//Dom Window [1]
 function openWindow() {
     window.open('https://www.linkedin.com/in/leslie-pelor-18102521b/');
+}
+
+function myFunction() {
+    window.open('https://github.com/lslgrcpelor/lab2');
 }
 
